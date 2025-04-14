f80ce21f59e330475afbf4acc3165cca934d5e87
Christos-Nik <78422437+Christos-Nik@users.noreply.github.com>
2025-04-14 18:25:35 +0300

    Create function_example.py

---
+++ Day 6/function_example.py
@@ -0,0 +1,7 @@
+#correct formatting to create a function, and how-to call it in main program. Careful with indentation.
+def my_function():   
+    print("Hello")
+    print("Bye")
+
+
+my_function()
