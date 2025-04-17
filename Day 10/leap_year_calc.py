dc8d780f71ee614b2b730526f5709bb2447bb9ec
Christos-Nik <78422437+Christos-Nik@users.noreply.github.com>
2025-04-17 21:32:30 +0300

    Create leap_year_calc.py

---
+++ Day 10/leap_year_calc.py
@@ -0,0 +1,17 @@
+def is_leap_year(year):
+    # Write your code here. 
+    leap_year = False
+    a = year % 4
+    if a == 0:
+        leap_year = True
+        b = year % 100
+        if b == 0:
+            leap_year = False
+            c = year % 400
+            if c == 0:
+                leap_year = True
+    
+    return leap_year
+    # Don't change the function name.
+
+print(is_leap_year(1700))    #should output True or False
