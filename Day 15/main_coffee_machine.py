a6646ab7a238f762579868cec733238249466897
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-19 23:27:28 +0300

    finished project of coffee machine

--- Day 15/main_coffee_machine.py
+++ Day 15/main_coffee_machine.py
@@ -35,35 +35,37 @@ while should_operate is True:
     if user_input == "off": #checks for the secret word for maintainers to turn off the coffee machine
         should_operate = False
     elif user_input == "report":
-        print(resources)
-    elif user_input == "espresso":
+        print(f"Water: {resources['water']} ml.")
+        print(f"Milk: {resources['milk']} ml.")
+        print(f"Coffee: {resources['coffee']} g.")
+    elif user_input in MENU: #checks if input is a valid option
         user_choice = MENU[user_input]
-        if user_choice[ingredients['water']] < resources['water']:
-            print("Sorry not enough water")
-        elif user_choice['milk'] < resources['milk']:
-            print("Sorry not enough milk.")
-        elif user_choice['coffee'] < resources['coffee']:
-            print("Sorry not enough coffee.")
-    elif user_input == "latte":
-        user_choice = MENU[user_input]
-        if user_choice['water'] < resources['water']:
-            print("Sorry not enough water")
-        elif user_choice['milk'] < resources['milk']:
-            print("Sorry not enough milk.")
-        elif user_choice['coffee'] < resources['coffee']:
-            print("Sorry not enough coffee.")
-    elif user_input == "cappuccino":
-        user_choice = MENU[user_input]
-        if user_choice['water'] < resources['water']:
-            print("Sorry not enough water")
-        elif user_choice['milk'] < resources['milk']:
-            print("Sorry not enough milk.")
-        elif user_choice['coffee'] < resources['coffee']:
-            print("Sorry not enough coffee.")
+        ingredients = user_choice["ingredients"]
+
+        can_make = True
+        for ingredient, amount in ingredients.items():
+            if resources.get(ingredient, 0) < amount:
+                print(f"Sorry, not enough {ingredient}.")
+                can_make = False
+                break 
+
+        if can_make is True:
+            print("Please insert coins.")
+            quarters = int(input("How many quarters?:\n")) * 0.25
+            dimes = int(input("How many dimes?:\n")) * 0.10
+            nickels = int(input("How many nickels?:\n")) * 0.05
+            pennies = int(input("How many pennies?:\n")) * 0.01
+            total_paid = quarters + dimes + nickels + pennies
+            cost = user_choice['cost']
+            if total_paid >= cost:
+                change = round(total_paid - cost, 2)
+                if change > 0:
+                    print(f"Here is your change: ${change}")
+                
+                print(f"Here is your {user_input}. Enjoy!")
+                for ingredient, amount in ingredients.items():
+                    resources[ingredient] -= amount
+            else:
+                print("Not enough money. Money refunded.")
     else:
-        print("Wrong input, try again.")    
-#TODO: 5. Process coins
-
-#TODO: 6. Check transaction successful? 
-
-#TODO: 7. Make coffee.
+        print("Wrong input, try again.")
