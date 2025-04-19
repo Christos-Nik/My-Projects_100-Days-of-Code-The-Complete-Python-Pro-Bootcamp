3aa73ccf53b3e7860c58ef8a664cb5bf083f326b
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-19 22:41:25 +0300

    new lines of code

--- Day 15/main_coffee_machine.py
+++ Day 15/main_coffee_machine.py
@@ -29,17 +29,20 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
-
+should_operate = True
 #TODO: 1. Prompt user by asking "What would you like? (espresso, latte, capuccino):"
-
+while should_operate is True:
+    user_input = input("What would you like? (espresso/latte/cappuccino):\n").lower()
 #TODO: 2. Turn off the Coffee Machine by entering "off" to the prompt.
-
+    if user_input == "off": #checks for the secret word for maintainers to turn off the coffee machine
+        should_operate = False
 #TODO: 3. Print report
-
+    elif user_input == "report":
+        print(resources)
 #TODO: 4. Check resources efficient?
-
+    elif user_input == "espresso":
 #TODO: 5. Process coins
 
 #TODO: 6. Check transaction successful? 
 
-#TODO: 7. Make coffee.
+#TODO: 7. Make coffee
