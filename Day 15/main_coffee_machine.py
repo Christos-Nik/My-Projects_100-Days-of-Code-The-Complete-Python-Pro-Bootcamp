e42f50b1978e04f2e3bb609771a4e747a44e4ca4
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-19 22:31:43 +0300

    added TODO list

--- Day 15/main_coffee_machine.py
+++ Day 15/main_coffee_machine.py
@@ -34,4 +34,12 @@ resources = {
 
 #TODO: 2. Turn off the Coffee Machine by entering "off" to the prompt.
 
-#TODO: 3. Print report.
+#TODO: 3. Print report
+
+#TODO: 4. Check resources efficient?
+
+#TODO: 5. Process coins
+
+#TODO: 6. Check transaction successful? 
+
+#TODO: 7. Make coffee.
