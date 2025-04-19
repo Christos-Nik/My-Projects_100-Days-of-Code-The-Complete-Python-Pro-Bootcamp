799c3565eb13ee0a716b087595573f93b85b1900
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-19 22:55:16 +0300

    tbc

--- Day 15/main_coffee_machine.py
+++ Day 15/main_coffee_machine.py
@@ -30,19 +30,40 @@ resources = {
     "coffee": 100,
 }
 should_operate = True
-#TODO: 1. Prompt user by asking "What would you like? (espresso, latte, capuccino):"
 while should_operate is True:
     user_input = input("What would you like? (espresso/latte/cappuccino):\n").lower()
-#TODO: 2. Turn off the Coffee Machine by entering "off" to the prompt.
     if user_input == "off": #checks for the secret word for maintainers to turn off the coffee machine
         should_operate = False
-#TODO: 3. Print report
     elif user_input == "report":
         print(resources)
-#TODO: 4. Check resources efficient?
     elif user_input == "espresso":
+        user_choice = MENU[user_input]
+        if user_choice[ingredients['water']] < resources['water']:
+            print("Sorry not enough water")
+        elif user_choice['milk'] < resources['milk']:
+            print("Sorry not enough milk.")
+        elif user_choice['coffee'] < resources['coffee']:
+            print("Sorry not enough coffee.")
+    elif user_input == "latte":
+        user_choice = MENU[user_input]
+        if user_choice['water'] < resources['water']:
+            print("Sorry not enough water")
+        elif user_choice['milk'] < resources['milk']:
+            print("Sorry not enough milk.")
+        elif user_choice['coffee'] < resources['coffee']:
+            print("Sorry not enough coffee.")
+    elif user_input == "cappuccino":
+        user_choice = MENU[user_input]
+        if user_choice['water'] < resources['water']:
+            print("Sorry not enough water")
+        elif user_choice['milk'] < resources['milk']:
+            print("Sorry not enough milk.")
+        elif user_choice['coffee'] < resources['coffee']:
+            print("Sorry not enough coffee.")
+    else:
+        print("Wrong input, try again.")    
 #TODO: 5. Process coins
 
 #TODO: 6. Check transaction successful? 
 
-#TODO: 7. Make coffee
+#TODO: 7. Make coffee.
