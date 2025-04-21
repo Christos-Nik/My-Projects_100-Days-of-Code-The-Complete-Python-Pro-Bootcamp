ac915d2bb3ebd985f2353248d7e78aa6f3b2e242
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-21 15:55:45 +0300

    added explanation line 18

--- Day 11/blackjack.py
+++ Day 11/blackjack.py
@@ -15,7 +15,7 @@ def calculate_score(cards):
     if sum(cards) == 21 and len(cards) == 2:
         return 0
 
-    if 11 in cards and sum(cards) > 21:
+    if 11 in cards and sum(cards) > 21: #checks if user's hand contain an ACE and depending if user is over 21 to count as 1
         cards.remove(11)
         cards.append(1)
 
