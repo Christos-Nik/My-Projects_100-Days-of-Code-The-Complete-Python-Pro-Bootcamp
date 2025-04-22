afb6f56380a6b24d089cae94513d76b2da5237b9
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-22 13:58:11 +0300

    coffee maker OOP project initialization

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -1,3 +1,14 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+
+should_operate = True
+while should_operate is True:
+    user_input = input("What would you like? (espresso/latte/cappuccino):\n").lower()
+    if user_input == "report":
+        print(coffee_maker.report)
+    elif user_input == "off":
+        should_operate = False
+        print("Machine is under maintenance.")
