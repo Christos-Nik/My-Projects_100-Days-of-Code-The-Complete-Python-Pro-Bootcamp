3ee9f84e5d5fdcfcbbf4608d970863b91a323828
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-22 14:13:44 +0300

    project initial draft

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -3,12 +3,22 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
 
 should_operate = True
+selection = menu.get_items()
+
 while should_operate is True:
     user_input = input("What would you like? (espresso/latte/cappuccino):\n").lower()
+    drink = menu.find_drink(user_input)
     if user_input == "report":
-        print(coffee_maker.report)
+        coffee_maker.report()
+        money_machine.report()
     elif user_input == "off":
         should_operate = False
         print("Machine is under maintenance.")
+    elif user_input in ("espresso", "latte", "cappuccino"):
+        if coffee_maker.is_resource_sufficient(drink) == True:
+            if money_machine.make_payment(drink.cost) == True:
+                coffee_maker.make_coffee(drink)
