be3dc88690d9cfd9671ecbbab6f2564289196216
Christos-Nik <78422437+Christos-Nik@users.noreply.github.com>
2025-04-24 19:58:03 +0300

    aaaa

--- Day 18/hirst-painting-start/main.py
+++ Day 18/hirst-painting-start/main.py
@@ -1,7 +1,6 @@
 ###This code will not work in repl.it as there is no access to the colorgram package here.###
 ##We talk about this in the video tutorials##
 import colorgram
-#de kserw ti kanw
 rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
 for color in colors:
