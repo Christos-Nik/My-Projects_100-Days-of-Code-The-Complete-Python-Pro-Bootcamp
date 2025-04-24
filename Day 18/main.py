b1bd074bf5562bc711042f709a11e182c3733881
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-24 12:05:01 +0300

    finished challenge 1 (100x100 square)

---
+++ Day 18/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("blue")
+
+# Challenge 1: Draw a square (100 x 100)
+# tim.fd(100)
+# tim.lt(90)
+# tim.fd(100)
+# tim.lt(90)
+# tim.fd(100)
+# tim.lt(90)
+# tim.fd(100)
+
+
+
+
+screen = Screen()
+screen.exitonclick()
