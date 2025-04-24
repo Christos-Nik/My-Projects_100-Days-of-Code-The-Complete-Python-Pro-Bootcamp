098b849301db1f000d0ecd71d5ab5076469f2d8e
Christos-Nik <78422437+Christos-Nik@users.noreply.github.com>
2025-04-25 00:01:28 +0300

    added explanation

--- Day 19/main.py
+++ Day 19/main.py
@@ -7,6 +7,6 @@ def move_forward():
     c.fd(10)
 
 
-screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.listen() # Set focus on TurtleScreen (in order to collect key-events).
+screen.onkey(key="space", fun=move_forward) # Bind function to key-release event of key. In this case, when user press "space" the turtle moves forward for 10 units
 screen.exitonclick()
