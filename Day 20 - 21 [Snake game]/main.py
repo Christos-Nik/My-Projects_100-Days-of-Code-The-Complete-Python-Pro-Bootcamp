b2c93042e0408731d49cb764f6bbaa6701e3dcfe
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-26 21:38:56 +0300

    added food, scoreboard

--- Day 20 - 21 [Snake game]/main.py
+++ Day 20 - 21 [Snake game]/main.py
@@ -1,6 +1,9 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
 screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
@@ -8,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -19,8 +24,15 @@ game_mode = True
 while game_mode:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
-        
+    #detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
+    #detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_mode = False
+        scoreboard.game_over()
+
 
 screen.exitonclick()
