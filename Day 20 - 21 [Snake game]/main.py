9ad99cec2cf4f57206592d3d80005e3e91fc0363
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-04-27 20:01:57 +0300

    added explanation to screen module usage

--- Day 20 - 21 [Snake game]/main.py
+++ Day 20 - 21 [Snake game]/main.py
@@ -13,7 +13,7 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
-
+# tell the screen to "listen" to certain key inputs, to move the snake 
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
