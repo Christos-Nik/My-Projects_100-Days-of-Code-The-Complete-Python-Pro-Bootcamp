28aab390ef7c14c00c418c5547eec0b4c7def73d
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2025-05-01 12:38:10 +0300

    end of day 23

--- Day 23 [turtle crossing game]/main.py
+++ Day 23 [turtle crossing game]/main.py
@@ -6,9 +6,36 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
+screen.title("Turtle Crossing")
 screen.tracer(0)
+
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.player_up, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    #Detect collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+    #Detect if player passed the stage
+    if player.finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+
+
+
+screen.exitonclick()
--- Day 23 [turtle crossing game]/player.py
+++ Day 23 [turtle crossing game]/player.py
@@ -1,7 +1,25 @@
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+from turtle import Turtle
 
+class Player(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
 
-class Player:
-    pass
+    def player_up(self):
+        self.fd(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def finish_line(self):
+       if self.ycor() > 280:
+           return True
+       else:
+           return False
