11a5c714286050cfcf1ab150bce4c47d767182d6
Christos Nikoloudakis <78422437+Christos-Nik@users.noreply.github.com>
2026-03-07 14:04:18 +0200

    new day files

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,3 +1,6 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
+with open("Day 24/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+with open("Day 24/my_file.txt", mode="a") as file:
+    file.write("\nnew text")
